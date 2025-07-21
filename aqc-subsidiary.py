f5d79f1fbecb07971712a5a5aa32f445fc0f60c8
beoutnaprun <133112969+beoutnaprun@users.noreply.github.com>
2025-07-21 17:37:22 +0800

    Update aqc-subsidiary.py

--- aqc-subsidiary.py
+++ aqc-subsidiary.py
@@ -18,8 +18,8 @@ def getConfig():
     return config
 
 proxy = {
-    'http': 'http://127.0.0.1:8082',
-    'https': 'http://127.0.0.1:8082',
+    # 'http': 'http://127.0.0.1:8082',
+    # 'https': 'http://127.0.0.1:8082',
 }
 
 header = {
